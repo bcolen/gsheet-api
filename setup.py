4c1ea0d30e8aa701ffb4e059c0f71bd7d0c1cc6e
Branden Colen <BrandenColen@brandens-mbp-2.lan>
2019-07-29 21:07:06 -0500

    fixed setup

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+import os
 from setuptools import setup
 
-setup(
-    name="gsheet_api",
-    version="0.1",
-    author="Branden Colen",
-    author_email="brandencolen@gmail.com",
-    description=("Easily and efficiently manage a Google Sheet"),
-    keywords="google sheet api pandas dataframe",
-    packages=['gsheet_api']
-)
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name="gsheet_api",
+      version="0.1",
+      author="Branden Colen",
+      author_email="brandencolen@gmail.com",
+      description="Easily and efficiently manage a Google Sheet",
+      long_description=read('README.md'),
+      keywords="google sheet api pandas dataframe easy",
+      packages=['gsheet_api'],
+      url="https://github.com/brandenc40/gsheet-api")
