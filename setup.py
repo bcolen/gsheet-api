94816d124edc0abb42fe8125b8197dda61af9ed0
Branden Colen <BrandenColen@brandens-mbp-2.lan>
2019-07-29 20:44:37 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gsheet_api",
+    version="0.1",
+    author="Branden Colen",
+    author_email="brandencolen@gmail.com",
+    description=("Easily and efficiently manage a Google Sheet"),
+    keywords="google sheet api pandas dataframe",
+    packages=['gsheet_api']
+)
