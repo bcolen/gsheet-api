bf77b75a0ac276cf3a785dab44c6500f17bc639f
Branden Colen <BrandenColen@brandens-mbp-2.lan>
2019-07-29 21:29:51 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
-import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
 setup(name="gsheet_api",
       version="0.1",
       author="Branden Colen",
       author_email="brandencolen@gmail.com",
-      description="Easily and efficiently manage a Google Sheet",
-      long_description=read('README.md'),
+      description=("Easily and efficiently manage a Google Sheet"),
       keywords="google sheet api pandas dataframe easy",
       packages=['gsheet_api'],
       url="https://github.com/brandenc40/gsheet-api")
